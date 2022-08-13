9078ca60e42b626d7082b69406f565a8cc50bf08
newSmile726 <1432072169@qq.com>
2022-08-13 14:03:42 +0800

    完成了css样式

--- src/module-manage/pages/menus.vue
+++ src/module-manage/pages/menus.vue
@@ -43,11 +43,6 @@ export default {
     return {
       tableData: [
         {
-          date: "2016-05-03",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
-        },
-        {
           date: "2016-05-02",
           name: "王小虎",
           address: "上海市普陀区金沙江路 1518 弄",
