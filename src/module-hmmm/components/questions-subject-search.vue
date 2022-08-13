06c5830d72bf6bb327674287680a2ae2afbd3f6d
baibaixy <2942583916@qq.com>
2022-08-13 14:12:53 +0800

    开始基础题库

--- src/module-hmmm/components/questions-subject-search.vue
+++ src/module-hmmm/components/questions-subject-search.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
+    <el-row type="flex" justify="end">
+      <el-button type="success" size="small">
+        <i class="el-icon-edit"></i>
+        <span>新增试题</span>
+      </el-button>
+    </el-row>
     <el-row type="flex">
-      <el-col></el-col>
       <el-col>
-        <el-row type="flex" justify="end">
-          <el-button type="success" size="small">
-            <i class="el-icon-edit"></i>
-            <span>新增试题</span>
-          </el-button>
-        </el-row>
+        <el-row type="flex"> </el-row>
+      </el-col>
+      <el-col>
+        <el-row type="flex"> </el-row>
       </el-col>
     </el-row>
   </div>
