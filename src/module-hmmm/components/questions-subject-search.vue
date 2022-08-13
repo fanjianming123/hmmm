a4dea60ac9fcdb0bcad17d0779d6783cdaa67aad
baibaixy <2942583916@qq.com>
2022-08-13 11:31:52 +0800

    提交一下

---
+++ src/module-hmmm/components/questions-subject-search.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <el-row type="flex">
+      <el-col></el-col>
+      <el-col>
+        <el-row type="flex" justify="end">
+          <el-button type="success" size="small">
+            <i class="el-icon-edit"></i>
+            <span>新增试题</span>
+          </el-button>
+        </el-row>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+
+  created() {},
+
+  methods: {},
+};
+</script>
+
+<style scoped></style>
